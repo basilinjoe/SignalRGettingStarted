1d3acfcf5134cbd6aa0bd2ff2eca8360a50d5f7d
Basilin Joe <basilin.joe@experionglobal.com>
2018-10-03 23:36:00 +0530

    initial commit

---
+++ clientDOTNET/Program.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace clientDOTNET
+{
+    class Program
+    {
+        private static SynchronizationContext Context;
+        private const string ADDRESS = "http://localhost:51030/signalr";
+        static void Main(string[] args)
+        {
+            bool terminate = false;
+            Console.WriteLine("Input UserName : ");
+            string userName = Console.ReadLine();
+            Context = SynchronizationContext.Current;
+            var hubConnection = new HubConnection(ADDRESS, useDefaultUrl: false);
+            IHubProxy chatHubProxy = hubConnection.CreateHubProxy("chatHub");
+            chatHubProxy["userName"] = userName;
+            chatHubProxy["computerName"] = "basilin-PC";
+            var notifyHandler = chatHubProxy.On("notify", (message) =>
+                Console.WriteLine(message)
+            );
+            var messageHandler = chatHubProxy.On("message", (message) =>
+                Console.WriteLine($": {message}")
+            );
+
+            /**
+             * Life Cycle Events.
+             * --------------------
+             * Received: Raised when any data is received on the connection. Provides the received data.
+             * ConnectionSlow: Raised when the client detects a slow or frequently dropping connection.
+             * Reconnecting: Raised when the underlying transport begins reconnecting.
+             * Reconnected: Raised when the underlying transport has reconnected.
+             * StateChanged: Raised when the connection state changes. Provides the old state and the new state.
+             * Closed: Raised when the connection has disconnected.
+             **/
+            hubConnection.Received += HubConnection_Received;
+            hubConnection.ConnectionSlow += HubConnection_ConnectionSlow;
+            hubConnection.Reconnecting += HubConnection_Reconnecting;
+            hubConnection.Reconnected += HubConnection_Reconnected;
+            hubConnection.StateChanged += HubConnection_StateChanged;
+            hubConnection.Closed += HubConnection_Closed;
+
+            /**
+             * Start Connection
+             **/
+            hubConnection.Start().Wait();
+
+            chatHubProxy.Invoke("send", "ConsoleApp", "Hello world").Wait();
+
+            while (!terminate)
+            {
+                string message = Console.ReadLine();
+                chatHubProxy.Invoke("send", userName, message).Wait();
+                if (message == "stop" || message == "exit") terminate = true;
+            }
+
+            notifyHandler.Dispose();
+            messageHandler.Dispose();
+            hubConnection.Stop();
+        }
+
+        #region Life Time Events
+        private static void HubConnection_Closed()
+        {
+            Console.WriteLine("HubConnection_Closed");
+        }
+
+        private static void HubConnection_StateChanged(StateChange obj)
+        {
+            //Console.WriteLine($"HubConnection_StateChanged from {obj.OldState} to {obj.NewState}");
+        }
+
+        private static void HubConnection_Reconnected()
+        {
+            Console.WriteLine("HubConnection_Reconnected");
+        }
+
+        private static void HubConnection_Reconnecting()
+        {
+            Console.WriteLine("HubConnection_Reconnecting");
+        }
+
+        private static void HubConnection_ConnectionSlow()
+        {
+            Console.WriteLine("HubConnection_ConnectionSlow");
+        }
+
+        private static void HubConnection_Received(string obj)
+        {
+            Console.WriteLine($"HubConnection_Received : {obj}");
+        }
+        #endregion
+    }
+}
---
+++ server/Hubs/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace server.Hubs
+{
+    [HubName("chatHub")]
+    public class ChatHub : Hub
+    {
+        public override Task OnConnected()
+        {
+            Clients.All.Notify($"Connected :{Context.ConnectionId.ToString()}");
+            return base.OnConnected();
+        }
+        public override Task OnReconnected()
+        {
+            Clients.All.Notify($"Reconnected :{Context.ConnectionId.ToString()}");
+            return base.OnReconnected();
+        }
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            Clients.All.Notify($"Disconnected :{Context.ConnectionId.ToString()}");
+            return base.OnDisconnected(stopCalled);
+        }
+        public void Send(string name, string message)
+        {
+            Clients.Others.Message($"{name}: {message}");
+        }
+    }
+}
